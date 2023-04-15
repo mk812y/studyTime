607b57880a1caf444e73d1627a2bf5a95e0ad5e1
mk812y <mk812y@gmail.com>
2023-04-15 16:40:49 +0200

    Initial Commit

---
+++ studyTime/studyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyTimeApp.swift
+//  studyTime
+//
+//  Created by Михаил Куприянов on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct studyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
